59833faeef7c5dd23677b4e8852c4a3b11cf4795
AishaMohamed83 <89880465+AishaMohamed83@users.noreply.github.com>
2026-02-04 19:40:52 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+let firstname = prompt("enter your name");
+let age = prompt("enter your age");
+let city = prompt("enter your city");
+
+document.getElementById("firstname").innerHTML = firstname;
+document.getElementById("age").innerHTML = age;
+document.getElementById("city").innerHTML = city;
